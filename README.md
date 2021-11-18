299aa95ff2da09bce84567744213102879b3ba36
sclarkstone <samclarkstone@hotmail.com>
2021-11-18 09:27:47 +0000

    connect flask to MongoDB

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 set up
+
+set up mongodb
 randomkeygen for secrete code
-
-
+create env file
+create .gitignore file
+create requirements file create procfile
+set up heroku - connect via github - add config vars - automatic deployment
+install flask pymongo
+install dnspython
+update requirements file
 
 
 ****** make sure to set debug to false when going live ********
--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Hello World ... again!"
+@app.route("/get_endings")
+def get_endings():
+    endings = mongo.db.endings.find()
+    return render_template("endings.html", endings=endings)
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
-            
